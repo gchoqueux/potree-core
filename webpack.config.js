0fad0cbb07e860db92256228e58b8c456675a9b9
gchoqueux <gerald.choqueux@ign.fr>
2020-10-28 11:53:02 +0100

    upgrade package

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         test: /Worker\.js$/,
         loader: 'worker-loader',
         include: [resolve('source')],
-        options: { inline: true, fallback: false },
+        options: { inline: 'no-fallback' },
       },
     ],
   },
